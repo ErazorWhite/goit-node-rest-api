28bd773efb3fc02d09c62a4a3be91c0524610d70
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-04-09 14:25:08 +0300

    path perfix /api/v1 added

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+const pathPrefix = "/api/v1";
+
+app.use(`${pathPrefix}/contacts`, contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
