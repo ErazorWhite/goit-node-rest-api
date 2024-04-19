c5858cc2235220e8fa0bba93cb7d56107503a8ab
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-04-19 17:57:19 +0300

    added CatchAsync for services

--- helpers/catchAsync.js
+++ helpers/catchAsync.js
@@ -1,5 +1,16 @@
-const catchAsync = (fn) => (req, res, next) => {
+import colors from "colors";
+
+export const catchAsync = (fn) => (req, res, next) => {
   fn(req, res, next).catch((err) => next(err));
 };
 
-export default catchAsync;
+export const catchAsyncService =
+  (fn) =>
+  async (...args) => {
+    try {
+      return await fn(...args);
+    } catch (error) {
+      console.error(colors.red("Error:"), error.message);
+      throw error;
+    }
+  };
