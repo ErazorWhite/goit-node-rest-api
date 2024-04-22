0daf257028127dffe4d284214369f96c6d66f42d
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-04-22 11:54:45 +0300

    +joiValidator schema for signup

---
+++ schemas/userSchemas.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+import { PASSWD_REGEX } from "../constants/regexp.js";
+
+export const registerUserSchema = Joi.object()
+  .options({ abortEarly: false })
+  .keys({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(PASSWD_REGEX)
+      .message(
+        "Password should have from 8 to 128 characters, at least one upper case letter (A-Z), at least one lower case letter (a-z), at least one digit (0-9), at least one special character."
+      )
+      .required(),
+  });
