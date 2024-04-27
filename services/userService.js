ec48693700b1281b29919d0bc5c3e9b99a5e127d
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-04-27 15:53:43 +0300

    added login & current user services

--- services/userService.js
+++ services/userService.js
@@ -3,14 +3,26 @@ import { catchAsyncService } from "../helpers/catchAsync.js";
 import { signToken } from "./jwtService.js";
 
 export const registerUserService = catchAsyncService(async (userData) => {
-  const newUser = await User.create(userData);
+  const { email, subscription } = await User.create(userData);
 
-  newUser.password = undefined;
-
-  const token = signToken(newUser.id);
-
-  return { newUser, token };
+  return { email, subscription };
 });
 
 export const checkUserExistsService = (email) => User.exists({ email });
 
+export const loginUserService = async ({ email, password }) => {
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) return null;
+
+  const isPasswordValid = await user.checkUserPassword(password, user.password);
+  if (!isPasswordValid) return null;
+
+  const token = signToken(user.id);
+
+  return {
+    user: { email: user.email, subscription: user.subscription },
+    token,
+  };
+};
+
+export const getUserByIdService = (id) => User.findById(id);
