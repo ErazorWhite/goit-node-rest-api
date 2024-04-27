e91c76e3961f4e806d4f62773911818106644157
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-04-27 15:54:11 +0300

    added login & current user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import HttpError from "../helpers/HttpError.js";
 import { catchAsync } from "../helpers/catchAsync.js";
 import {
   checkUserExistsService,
+  loginUserService,
   registerUserService,
 } from "../services/userService.js";
 
@@ -11,11 +12,26 @@ export const registerUser = catchAsync(async (req, res) => {
   const userExists = await checkUserExistsService(user.email);
   if (userExists) throw HttpError(409, "Email in use");
 
-  const { newUser, token } = await registerUserService(user);
-  if (!newUser) throw HttpError();
+  const newUser = await registerUserService(user);
+  if (!newUser) throw HttpError(500);
 
   res.status(201).json({
     user: newUser,
+  });
+});
+
+export const loginUser = catchAsync(async (req, res) => {
+  const { user, token } = await loginUserService(req.body);
+  if (!user || !token) throw HttpError(401);
+
+  res.status(200).json({
+    user,
     token,
   });
 });
+
+export const currentUser = (req, res) => {
+  res.status(200).json({
+    user: { email: req.user.email, subscription: req.user.subscription },
+  });
+};
