a8a3aafaba073c0407039b3c5552bb4cfa044be7
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-02 11:21:11 +0300

    initUploadImageMiddleware refactored, code moved to ImageService

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,12 +1,8 @@
-import multer from "multer";
-import path from "path";
-
 import HttpError from "../helpers/HttpError.js";
 import { catchAsync } from "../helpers/catchAsync.js";
+import { ImageService } from "../services/imageService.js";
 import { checkToken } from "../services/jwtService.js";
 import { getUserByIdService } from "../services/userService.js";
-import { v4 } from "uuid";
-import { MB_SIZE } from "../constants/customValues.js";
 
 export const protect = catchAsync(async (req, res, next) => {
   const token =
@@ -24,33 +20,4 @@ export const protect = catchAsync(async (req, res, next) => {
   next();
 });
 
-// MULTER
-// config storage
-const multerStorage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, path.join("public", "avatars"));
-  },
-  filename: (req, file, callback) => {
-    const extension = file.mimetype.split("/")[1];
-
-    callback(null, `${req.user.id}-${v4()}.${extension}`);
-  },
-});
-
-// config filter
-const multerFilter = (req, file, callback) => {
-  if (file.mimetype.startsWith("image/")) {
-    callback(null, true);
-  } else {
-    callback(HttpError(400, "Please upload images only"), false);
-  }
-};
-
-// create middleware
-export const uploadAvatar = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
-  limits: {
-    fieldSize: 2 * MB_SIZE,
-  },
-}).single('avatar');
+export const uploadAvatar = ImageService.initUploadImageMiddleware("avatar");
