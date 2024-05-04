14b78d29f181dfde311755c0c0e6572ce4bf7564
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-05-04 22:29:40 +0300

    email validation process implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,18 @@
+import { v4 } from "uuid";
+
 import HttpError from "../helpers/HttpError.js";
 import { catchAsync } from "../helpers/catchAsync.js";
 import {
   checkUserExistsService,
+  findUserByEmailService,
   loginUserService,
   logoutUserService,
   registerUserService,
-  updateCurrentUserService,
+  updateCurrentUserAvatarService,
   updateSubscriptionUserService,
+  verifyUserByVerificationToken,
 } from "../services/userService.js";
+import { EmailService } from "../services/emailService.js";
 
 export const registerUser = catchAsync(async (req, res) => {
   const user = req.body;
@@ -15,8 +20,15 @@ export const registerUser = catchAsync(async (req, res) => {
   const userExists = await checkUserExistsService(user.email);
   if (userExists) throw HttpError(409, "Email in use");
 
+  user.verificationToken = v4();
+
   const newUser = await registerUserService(user);
   if (!newUser) throw HttpError(500);
+
+  const verificationURL = `${process.env.BASE_URL}/users/verify/${user.verificationToken}`;
+
+  const emailService = new EmailService(newUser, verificationURL);
+  await emailService.sendVerificationEmail(verificationURL);
 
   res.status(201).json({
     user: newUser,
@@ -64,8 +76,36 @@ export const currentUser = (req, res) => {
 };
 
 export const updateCurrentUser = catchAsync(async (req, res) => {
-  const updatedUser = await updateCurrentUserService(req.user, req.file);
+  const updatedUser = await updateCurrentUserAvatarService(req.user, req.file);
   res.status(200).json({
     avatarURL: updatedUser.avatarURL,
   });
+});
+
+export const verifyToken = catchAsync(async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await verifyUserByVerificationToken(verificationToken);
+
+  if (!user) throw HttpError(404, "User not found");
+
+  res.status(200).json({ message: "Verification successful" });
+});
+
+export const resendVerificationEmail = catchAsync(async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) throw HttpError(400, "Missing required field email");
+
+  const user = await findUserByEmailService(email);
+
+  if (!user) throw HttpError(404, "User not found");
+
+  if (user.verify) throw HttpError(400, "Verification has already been passed");
+
+  const verificationURL = `${process.env.BASE_URL}/users/verify/${user.verificationToken}`;
+
+  const emailService = new EmailService(user, verificationURL);
+  await emailService.sendVerificationEmail(verificationURL);
+
+  res.status(200).json({ message: "Verification email sent" });
 });
